741f79a42bf8b8a0a61fe443d68c860212d1b257
ReTRAI <geral@retrai.co>
2022-09-23 14:20:11 +0000

    Replace etar with calendar

--- config/common_mobile.mk
+++ config/common_mobile.mk
@@ -9,15 +9,11 @@ PRODUCT_PRODUCT_PROPERTIES += \
 # Apps
 PRODUCT_PACKAGES += \
     Backgrounds \
-    Etar \
+    Calendar \
     ExactCalculator \
     Profiles \
     Seedvault
 
-ifneq ($(TARGET_EXCLUDES_AUDIOFX),true)
-PRODUCT_PACKAGES += \
-    AudioFX
-endif
 
 ifeq ($(PRODUCT_TYPE), go)
 PRODUCT_PACKAGES += \
