9eee0f38fc90cb797f5ffca1f3ff50c6cc8f0e1e
PortalRomOS <portalromOS@gmail.com>
2023-01-11 12:12:34 +0000

    change soong generator

--- build/soong/generator/variables.go
+++ build/soong/generator/variables.go
@@ -6,12 +6,12 @@ import (
 	"android/soong/android"
 )
 
-func lineageExpandVariables(ctx android.ModuleContext, in string) string {
-	lineageVars := ctx.Config().VendorConfig("lineageVarsPlugin")
+func portalromExpandVariables(ctx android.ModuleContext, in string) string {
+	portalromVars := ctx.Config().VendorConfig("portalromVarsPlugin")
 
 	out, err := android.Expand(in, func(name string) (string, error) {
-		if lineageVars.IsSet(name) {
-			return lineageVars.String(name), nil
+		if portalromVars.IsSet(name) {
+			return portalromVars.String(name), nil
 		}
 		// This variable is not for us, restore what the original
 		// variable string will have looked like for an Expand
