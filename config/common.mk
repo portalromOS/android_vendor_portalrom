c7266fb3e0c41aa7564890d889656a15392861c3
PortalRomOS <portalromOS@gmail.com>
2023-01-23 15:27:34 +0000

    Correct references to setupwizard

--- config/common.mk
+++ config/common.mk
@@ -204,7 +204,7 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/crowdin/overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/crowdin/overlay
 
 PRODUCT_EXTRA_RECOVERY_KEYS += \
-    vendor/portalrom/build/target/product/security/lineage
+    vendor/portalrom/build/target/product/security/portalrom
 
 include vendor/portalrom/config/version.mk
 
